7def15162bd2cef66c784cc0e527f1016f5dd118
Paul Knopf <pauldotknopf@gmail.com>
2016-05-27 22:50:56 -0400

    Added Mvc5 sample project.

---
+++ src/Sample.Mvc5/Global.asax.cs
@@ -0,0 +1,16 @@
+﻿using JavaScriptViewEngine;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Sample.Mvc5
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            ViewEngines.Engines.Add(new JsViewEngine(new Options<JsViewEngineOptions>(new JsViewEngineOptions())));
+        }
+    }
+}
